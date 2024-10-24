413d943daa5790827b55d932b02226dae210d011
nizzynas <nas@nvs.tech>
2024-10-24 12:28:38 -0400

    initial commit

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match with our records.',
+    'inactive' => 'Your account deactivated, Please contact to site owner.',
+    'verification' => 'Your account not verified yet, Please verify first.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
---
+++ resources/lang/es/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return array (
+  'language' => 'idioma',
+  'tuesday' => 'Tuesday',
+  'wednesday' => 'Wednesday',
+  'thursday' => 'Thursday',
+  'friday' => 'Friday',
+  'saturday' => 'Saturday',
+  'sunday' => 'Sunday',
+  'monday' => 'Monday',
+);
